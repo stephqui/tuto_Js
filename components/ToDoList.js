b4af4c30314327d3cba8d7891a02ce54e39259e2
Stéphane PROST-TOURNIER <stephane.pt@solution-logique.local>
2025-06-27 16:56:01 +0200

    Class ToDo à construire

--- components/ToDoList.js
+++ components/ToDoList.js
@@ -1,3 +1,14 @@
-export class ToDoList{
-    
+export class ToDoList {
+    liClass = 'todo list-group-item d-flex align-items-center'
+    inputClass = 'form-check-input'
+    inputType='checkbox'
+    label1Class='ms-2 form-check-label'
+    iClass='bi-trash'
+    label2Class='ms-auto btn btn-danger btn-sm'
+
+    constructor(li, input,label1, label2, i) {
+        this.label1=label1
+        this.label2=label2
+    }
+
 }
