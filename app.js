d7b40ad3a4a24ec5d12c29e360cebcd8002e96e6
Stéphane PROST-TOURNIER <stephane.pt@solution-logique.local>
2025-07-21 16:29:34 +0200

    Test todos2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { fetchSON } from "./functions/api.js"
 import { createElement } from "./functions/dom.js"
 
 try {
-    //const todos = await fetchSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    const todos2 = await fetchSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
     const todosInStorage = localStorage.getItem('lesTodos')?.toString()
     let todos=[]
     if(todosInStorage){
